5f3589f5dd0025c4e110dc50423a20caecfab384
sandeepkaran <https://github.com/sandeepNoida7>
2025-09-10 02:05:13 +0530

    first commkit

--- tests/MyWebApi.Tests/obj/Debug/net6.0/MyWebApi.Tests.AssemblyInfo.cs
+++ tests/MyWebApi.Tests/obj/Debug/net6.0/MyWebApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8c317fe2ef72be7104ac8fd62f4f4d06b769ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1359096ce37eaca5c57af84182ca21a6247d054")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
